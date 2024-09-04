16b241285f3f6c3031cbf80278ddb0b23b7d8cc3
isalin <isa@teste.com>
2024-09-04 15:14:01 -0300

    iniciando responsividade

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded', function(){
     const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if(posicaoAtual<alturaHero){
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
+
+    //Seção FAQ
     for(let i = 0 ; i<questions.length; i++){
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
+
 })
 
 function abreOuFechaResposta(elemento){
@@ -11,4 +25,14 @@ function abreOuFechaResposta(elemento){
     const elementoPai = elemento.target.parentNode;
 
     elementoPai.classList.toggle(classe);
+}
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
